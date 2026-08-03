46cf4b392035c0ad1ae7a2bab839f2c5c771a0ef
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 14:41:33 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/yard/yaml.rb
+++ lib/yard/yaml.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-require "version_gem"
 require_relative "yaml/version"
-
-Yard::Yaml::Version.class_eval do
-  extend VersionGem::Basic
-end
 
 require_relative "yaml/config"
 require_relative "yaml/cli"
