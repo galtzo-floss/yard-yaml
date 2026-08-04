8468ecc05cf72cc85691faa70cbab5f00c8ece72
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 19:31:21 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/yard/yaml/version_spec.rb
+++ spec/yard/yaml/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "anonymous_loader"
-require "yard-yaml"
+require "yard/yaml"
 RSpec.describe Yard::Yaml::Version do
   it_behaves_like "a Version module", described_class
 
