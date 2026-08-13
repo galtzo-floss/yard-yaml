76d849027b4fefb30f3606c68d26dc758f9f4b3d
Peter H. Boling <peter.boling@gmail.com>
2026-08-13 05:20:26 -0600

    🔖 Bump gem version

--- lib/yard/yaml/version.rb
+++ lib/yard/yaml/version.rb
@@ -5,7 +5,7 @@ module Yard
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "0.2.5"
+      VERSION = "0.2.6"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
